62a17adc4069c28bd5c4cbc9006c49ab620357b9
hunzi2636 <114270947+hunzi2636@users.noreply.github.com>
2023-03-06 21:12:19 +0800

    Delete name.php

--- name.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli=require 'db.php';
-$sql = "SELECT * FROM user WHERE phone =".$_POST['phone']."";
-$res=mysqli_query($mysqli,$sql);
-$row = mysqli_fetch_assoc($res);
-$name=$row['name'];
-return "$name";
-$mysqli->close();
